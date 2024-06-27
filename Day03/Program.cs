a7c826fa763c622db9716c3c4e5779ec44f1ed56
ibrahymMahmoud <ibrahymmahmoud123@gmail.com>
2024-06-27 20:45:28 +0300

    control Statements 1.Conditional Statements [if , switch] Equality

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -5,7 +5,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region control Statements 1.Conditional Statements [if , switch] Equality
+            //Q.check if month number in first quarter using if
+            ///   Console.Write("please enter an month numbers existed in 1st quarter: ");
+            ///   int.TryParse(Console.ReadLine(), out int MonthNumber);
+            ///   if(MonthNumber == 1)
+            ///   {
+            ///       Console.WriteLine("hello january");
+            ///   }
+            ///   else if (MonthNumber == 2)
+            ///   {
+            ///       Console.WriteLine("hello febraury");
+            ///   }
+            ///   else if (MonthNumber == 3)
+            ///   {
+            ///       Console.WriteLine("hello march");
+            ///   }
+            ///   else
+            ///       Console.WriteLine("month not in first quarter");
+
+            //Q.check if month number in first quarter using switch
+            ///     Console.Write("please enter an month numbers existed in 1st quarter: ");
+            ///     int.TryParse(Console.ReadLine(), out int MonthNumber);
+            ///  switch (MonthNumber)
+            ///  {
+            ///
+            ///      case 1:
+            ///          Console.WriteLine("hello january");
+            ///          break;
+            ///      case 2:
+            ///          Console.WriteLine("hello febrauary");
+            ///          break;
+            ///      case 3:
+            ///          Console.WriteLine("hello march");
+            ///          break;
+            ///      default:
+            ///          Console.WriteLine("entered month not in first quarter");
+            ///          break;
+            ///  }
+
+            /*
+             * when switching on varible of type numeric
+             * compiler will generate jump table 
+             * jump table consist of (uniqe key , value )
+             * it take 1 step in compile time (more performance)*/
+
+            #endregion
+
+
+
         }
     }
 }
