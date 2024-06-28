9eca29657164c08f6c9e6212388df1c0d2684725
ibrahymMahmoud <ibrahymmahmoud123@gmail.com>
2024-06-28 08:54:50 +0300

    2- Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -11,20 +11,40 @@ namespace Assignment
             // then print yes if that number can be divided by 3 and 4 otherwise print no.
 
 
-        ///   int Number;
-        ///   bool flag;
-        ///   do
-        ///   {
-        ///       Console.Write("Enter the Number: ");
-        ///       flag = int.TryParse(Console.ReadLine(), out Number);
-        ///   } while (!flag);
-        ///   if( Number%3 == 0 && Number % 4 == 0)
-        ///   {
-        ///       Console.WriteLine("yes,the number can divded on 3 and 4 ");
-        ///   } else
-        ///       Console.WriteLine("no,the number can't divded on 3 and 4 ");
+            ///   int Number;
+            ///   bool flag;
+            ///   do
+            ///   {
+            ///       Console.Write("Enter the Number: ");
+            ///       flag = int.TryParse(Console.ReadLine(), out Number);
+            ///   } while (!flag);
+            ///   if( Number%3 == 0 && Number % 4 == 0)
+            ///   {
+            ///       Console.WriteLine("yes,the number can divded on 3 and 4 ");
+            ///   } else
+            ///       Console.WriteLine("no,the number can't divded on 3 and 4 ");
 
             #endregion
+
+            #region Q2
+            //2- Write a program that allows the user to insert an integer
+            //then print negative if it is negative number otherwise print positive.
+
+            ///   int Number;
+            ///   bool flag;
+            ///   do
+            ///   {
+            ///       Console.Write("Enter the Number: ");
+            ///       flag = int.TryParse(Console.ReadLine(), out Number);
+            ///   } while (!flag);
+            ///   if( Number<0)
+            ///   {
+            ///       Console.WriteLine("it is negative number ");
+            ///   } else
+            ///       Console.WriteLine("it is positive number ");
+            #endregion
+
+
         }
     }
 }
